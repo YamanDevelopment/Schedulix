1fff6525f974e9dac1a3e439d373e3bf2fac3c35
Thandi M. <TMenelas@outlook.com>
2023-10-22 22:52:17 -0400

    Imported Zach's frontend implementation of the solver @ZachLTech

---
+++ src/frontend/README.md
@@ -0,0 +1,31 @@
+# Schedulix (frontend)
+
+Your All-In-One College Schedule Solution! Why spend countless hours forming a schedule when you can have Schedulix form multiple better schedules for you- and according to your preferences!
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+
+## Installation
+
+If you wanna try using the frontend yourself, you can clone the repository, open up a terminal and cd into the project folder then type 
+```bash
+npm install
+```
+and once that's complete type
+```bash
+npm run dev
+```
+And it should return a localhost link for you to visit and boom! You're viewing the websites frontend! enjoy. 😊
+
+## Usage
+
+In a real world scenario, if or when the project is finalized beyond it's Hackathon submission state, students who would want to make a schedule would simply go onto the website, hit start, and follow the outlined steps to generating a schedule that matches their wants, needs, and general schedule preferences to match them and their perfectly semester. 
+
+## Contributing
+
+If anyone dares to, you can fork this repository and contribute if you'd like. To do so just follow the instructions in [Installation](#installation) but clone your forked repository instead so you can commit any changes you make or work you do.
+
+
+WE WON THE HACKATHONNN!!!!!!! TOP PRIZE FIRST HACKATON!!!
---
+++ src/frontend/src/lib/Info.svelte
@@ -0,0 +1,57 @@
+<script>
+	import logo from "$lib/assets/logo.png"
+</script>
+
+<div class="h-full w-screen mx-auto flex justify-center items-center p-12">
+	<div class="w-full h-full rounded-3xl flex flex-col gap-3">
+        <div id="top" class="bg-surface-700 mb-3 w-full h-1/5 rounded-3xl flex flex-col justify-center p-8 gap-5">
+            <h3 class="text-4xl text-primary-500 font-semibold">What is this?</h3>
+            <p class="text-2xl">A user-friendly web application designed for college students to efficiently organize their academic schedules. By inputting their preferred courses, the app accesses real-time course data to propose feasible class schedules, ensuring minimal conflicts and maximum convenience for the student.</p>
+        </div>
+        <div id="bottom" class="flex mt-6 w-full h-4/5 gap-10">
+            <div id="rn" class="w-2/4 h-full bg-surface-700 rounded-3xl flex flex-col p-8 gap-8">
+                <h3 class="text-5xl text-primary-500 font-semibold">What it can do right now</h3>
+                <ol class="list">
+                    <li>
+                        <span class="text-2xl">•</span>
+                        <span class="flex-auto text-2xl">Take input classes planning to be taken and organize them into possible schedules for a semester</span>
+                    </li>
+                    <br>
+                    <li>
+                        <span class="text-2xl">•</span>
+                        <span class="flex-auto text-2xl">Take inputs for preferred excluded times in the generated schedules to make them follow your time preferences</span>
+                    </li>
+                </ol>
+            </div>
+            <div id="future" class="w-2/4 h-full bg-surface-700 rounded-3xl flex flex-col p-8 gap-8">
+                <h3 class="text-5xl text-primary-500 font-semibold">Future plans/ potential features</h3>
+                <ol class="list">
+                    <li>
+                        <span class="text-2xl">•</span>
+                        <span class="flex-auto text-2xl">Preference for particularly early or late classes (night owls or early birds)</span>
+                    </li>
+                    <br>
+                    <li>
+                        <span class="text-2xl">•</span>
+                        <span class="flex-auto text-2xl">Preference for a particular weekday off to have study day/extra day off</span>
+                    </li>
+                    <br>
+                    <li>
+                        <span class="text-2xl">•</span>
+                        <span class="flex-auto text-2xl">Preference for shorter, longer, or varied breaks between classes</span>
+                    </li>
+                    <br>
+                    <li>
+                        <span class="text-2xl">•</span>
+                        <span class="flex-auto text-2xl">RateMyProfessor integration for optimal classes considering professor ratings</span>
+                    </li>
+                    <br>
+                    <li>
+                        <span class="text-2xl">•</span>
+                        <span class="flex-auto text-2xl">Feature to block early morning or late classes from being added to schedule solutions</span>
+                    </li>
+                </ol>
+            </div>
+        </div>
+    </div>
+</div>
